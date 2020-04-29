d2de9c803ef6602a37a175e1a49b1eb393829660
mdkhau <git>
2020-04-30 00:11:20 +0300

    Updates

---
+++ precache-manifest.d28b212f00d6153e2d95413a479d21f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d6106c141b4e3232313cefddd66b12",
+    "url": "/word-app/index.html"
+  },
+  {
+    "revision": "18a5635ac28f0282e17a",
+    "url": "/word-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "95b17192381531a03daf",
+    "url": "/word-app/static/js/2.4f488296.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/word-app/static/js/2.4f488296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a5635ac28f0282e17a",
+    "url": "/word-app/static/js/main.bcbf30c8.chunk.js"
+  },
+  {
+    "revision": "3fb71b8c1d3a4eb85128",
+    "url": "/word-app/static/js/runtime-main.b8654bac.js"
+  }
+]);
